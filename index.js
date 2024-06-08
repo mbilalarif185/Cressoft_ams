5cfdd796b0157fd953fff6cc5c84b9b69a153522
Muhammad Bilal Arif <mbilalarif185@gmail.com>
2024-06-08 16:58:16 +0500

    versel config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 const app=express();
 const port = 9000;
 app.use('/',(req,res)=>{
-    res.json({message:" Hey I am deploying it on Vercel"})
+    res.send(" Hey I am deploying it on Vercel")
 })
 app.listen(9000,()=>{
     console.log('Server is running on port:', port);
