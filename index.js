65d7a193e51999e6db4306ac7d34f90cc9689eb6
Muhammad Bilal Arif <mbilalarif185@gmail.com>
2024-06-21 01:05:58 +0500

    versel config

--- index.js
+++ index.js
@@ -76,6 +76,7 @@ app.post("/loginAction", async (req, res) => {
 ///leave management system
 app.get("/apply_leave", (req, res) => {
   const user = req.session.user;
+  console.log(user.name)
   if (user) {
     res.render("apply_leave", { userName: user.name });
   } else {
@@ -111,99 +112,6 @@ app.post("/apply_leave", async (req, res) => {
   }
 });
 
-
-// app.get("/leave_history", async (req, res) => {
-//   const user = req.session.user;
-//   console.log("User session:", user);  // Debugging line
-
-//   if (user) {
-//     try {
-//       const query = `
-//         SELECT leave_id, user_id, start_date, end_date, reason, status
-//         FROM leaves
-//         WHERE user_id = $1
-//         ORDER BY start_date DESC;
-//       `;
-//       const values = [user.id];
-//       const result = await pool.query(query, values);
-//       const leaves = result.rows;
-      
-//       // Count approved and rejected leaves
-//       const approvedLeaves = leaves.filter(leave => leave.status === 'Approved').length;
-//       const rejectedLeaves = leaves.filter(leave => leave.status === 'Rejected').length;
-
-//       res.render("leave_history", { userName: user.name, leaves, approvedLeaves, rejectedLeaves });
-//     } catch (error) {
-//       console.error("Error fetching leave records:", error);
-//       res.status(500).send("Internal Server Error");
-//     }
-//   } else {
-//     res.redirect("/");
-//   }
-// });
-// app.get("/review_leaves", async (req, res) => {
-//   const user = req.session.user;
-//   console.log("User session:", user);  // Debugging line
-
-//   if (user && user.role.trim().toLowerCase() === 'developer') {
-//     try {
-//       const query = `
-//         SELECT leave_id, user_id, start_date, end_date, reason, status
-//         FROM leaves
-//         WHERE status = 'Pending'
-//         ORDER BY start_date DESC;
-//       `;
-//       const result = await pool.query(query);
-//       const leaves = result.rows;
-      
-//       // Fetch all users and their leave counts
-//       const usersQuery = `
-//         SELECT id, name FROM login
-//       `;
-//       const usersResult = await pool.query(usersQuery);
-//       const users = usersResult.rows;
-
-//       // Initialize leaveCounts object
-//       const leaveCounts = {};
-
-//       // Calculate approved and rejected leave days for each user
-//       for (let user of users) {
-//         leaveCounts[user.id] = {
-//           approvedLeaveDays: 0,
-//           rejectedLeaveDays: 0
-//         };
-
-//         const userLeavesQuery = `
-//           SELECT start_date, end_date, status
-//           FROM leaves
-//           WHERE user_id = $1
-//         `;
-//         const userLeavesValues = [user.id];
-//         const userLeavesResult = await pool.query(userLeavesQuery, userLeavesValues);
-//         const userLeaves = userLeavesResult.rows;
-
-//         userLeaves.forEach(leave => {
-//           const startDate = new Date(leave.start_date);
-//           const endDate = new Date(leave.end_date);
-//           const leaveDays = (endDate - startDate) / (1000 * 60 * 60 * 24) + 1; // Calculate total days of leave
-
-//           if (leave.status === 'Approved') {
-//             leaveCounts[user.id].approvedLeaveDays += leaveDays;
-//           } else if (leave.status === 'Rejected') {
-//             leaveCounts[user.id].rejectedLeaveDays += leaveDays;
-//           }
-//         });
-//       }
-
-//       res.render("review_leaves", { userName: user.name, leaves, users, leaveCounts });
-//     } catch (error) {
-//       console.error("Error fetching leave records:", error);
-//       res.status(500).send("Internal Server Error");
-//     }
-//   } else {
-//     res.redirect("/");
-//   }
-// });
 
 app.get("/leave_history", async (req, res) => {
   const user = req.session.user;
@@ -475,128 +383,6 @@ app.get("/monthly_working_hours", async (req, res) => {
     res.redirect("/");
   }
 });
-
-// app.post('/monthly_working_hours', async (req, res) => {
-//   const { name, year, month } = req.body;
-//   const user = req.session.user;
-//   const loginId = user.id;
-
-//   const daysInMonth = new Date(year, month, 0).getDate();
-  
-//   try {
-//     const monthlyData = [];
-//     let NAME='';
-
-//     for (let day = 1; day <= daysInMonth; day++) {
-//       const date = `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
-
-//       const query = `
-//         SELECT name,check_in_time, check_out_time, reason,date
-//         FROM record
-//         WHERE (
-//           lower(name) LIKE lower($1) OR
-//           lower(name) LIKE lower($2) OR
-//           lower(name) LIKE lower($3)
-//         )
-//         AND date = $4 AND id = $5
-//         ORDER BY check_in_time;
-//       `;
-//       // const values = [name, date, loginId];
-//       const values = [
-//         `%${name.split(' ')[0]}%`, // First part of the name
-//         `%${name.split(' ')[1]}%`, // Middle part of the name
-//         `%${name.split(' ')[2]}%`, // Last part of the name
-//         date,
-//         loginId
-//       ];
-//       const result = await pool.query(query, values);
-     
-//       if (result.rows.length > 0) {
-//         let totalMinutes = 0;
-//         let breakMinutes = 0;
-//         let officeWorkMinutes = 0;
-//         let otherMinutes = 0;
-
-//         for (let i = 0; i < result.rows.length; i++) {
-//           const row = result.rows[i];
-//           const checkInTime = row.check_in_time;
-//           NAME=row.name;
-//           const checkOutTime = row.check_out_time;
-//           const reason = row.reason.trim().toLowerCase(); // Trim and lower case the reason
-
-//           if (checkInTime && checkOutTime) {
-//             const checkInDateTime = new Date(`1970-01-01T${checkInTime}`);
-//             const checkOutDateTime = new Date(`1970-01-01T${checkOutTime}`);
-//             const diffMs = checkOutDateTime - checkInDateTime;
-//             const diffMinutes = diffMs / 1000 / 60;
-
-//             totalMinutes += diffMinutes;
-//           }
-
-//           if (checkOutTime && i < result.rows.length - 1) {
-//             const nextRow = result.rows[i + 1];
-//             const nextCheckInTime = nextRow.check_in_time;
-//             if (nextCheckInTime) {
-//               const checkOutDateTime = new Date(`1970-01-01T${checkOutTime}`);
-//               const nextCheckInDateTime = new Date(`1970-01-01T${nextCheckInTime}`);
-//               const diffMs = nextCheckInDateTime - checkOutDateTime;
-//               const diffMinutes = diffMs / 1000 / 60;
-
-//               if (reason === 'break') {
-//                 breakMinutes += diffMinutes;
-//               } else if (reason.includes('office')) { // Check if reason contains "office"
-//                 officeWorkMinutes += diffMinutes;
-//               } else {
-//                 otherMinutes += diffMinutes;
-//               }
-//             }
-//           }
-//         }
-
-//         const totalHours = Math.floor(totalMinutes / 60);
-//         const totalMinutesRemainder = totalMinutes % 60;
-
-//         const breakHours = Math.floor(breakMinutes / 60);
-//         const breakMinutesRemainder = breakMinutes % 60;
-
-//         const officeWorkHours = Math.floor(officeWorkMinutes / 60);
-//         const officeWorkMinutesRemainder = officeWorkMinutes % 60;
-
-//         const otherHours = Math.floor(otherMinutes / 60);
-//         const otherMinutesRemainder = otherMinutes % 60;
-//         console.log("Name",NAME)
-//         monthlyData.push({
-//           NAME,
-//           date,
-//           totalHours,
-//           totalMinutesRemainder,
-//           breakHours,
-//           breakMinutesRemainder,
-//           officeWorkHours,
-//           officeWorkMinutesRemainder,
-//           otherHours,
-//           otherMinutesRemainder
-//         });
-//       }
-//     }
-//     const months = [
-//       'January', 'February', 'March', 'April', 'May', 'June',
-//       'July', 'August', 'September', 'October', 'November', 'December'
-//     ];
-  
-//     let monthname = '';
-//     if (month >= 1 && month <= 12) {
-//       monthname = months[month - 1]; // Months array is zero-based
-//     } else {
-//       throw new Error('Invalid month number');
-//     }
-//     console.log("Monthly Data:",monthlyData)
-//     res.render('monthly_working_hours_report', {year, monthname, monthlyData});
-//   } catch (error) {
-//     console.error("Error executing query:", error);
-//     res.status(500).send("Internal Server Error");
-//   }
-// });
 app.post('/monthly_working_hours', async (req, res) => {
   const { name, year, month } = req.body;
   const user = req.session.user;
@@ -748,116 +534,6 @@ app.get("/daily_working_hours", async (req, res) => {
     res.redirect("/");
   }
 });
-
-
-// app.post('/daily_working_hours', async (req, res) => {
-//   const { name, date } = req.body;
-//   const user = req.session.user;
-//   const loginId = user.id;
-//   let NAME=''
-//   try {
-//     const query = `
-//       SELECT name,check_in_time, check_out_time, reason
-//       FROM record
-//       WHERE (
-//           lower(name) LIKE lower($1) OR
-//           lower(name) LIKE lower($2) OR
-//           lower(name) LIKE lower($3)
-//         )
-//       AND date = $4 AND id = $5
-//       ORDER BY check_in_time;
-//     `;
-//     const values = [
-//         `%${name.split(' ')[0]}%`, // First part of the name
-//         `%${name.split(' ')[1]}%`, // Middle part of the name
-//         `%${name.split(' ')[2]}%`, // Last part of the name
-//         date,
-//         loginId
-//       ];
-    
-//     const result = await pool.query(query, values);
-//       console.log(result)
-//     if (result.rows.length === 0) {
-//       res.status(404).send("No records found for the specified date.");
-//       return;
-//     }
-
-//     let totalMinutes = 0;
-//     let breakMinutes = 0;
-//     let officeWorkMinutes = 0;
-//     let otherMinutes = 0;
-
-//     for (let i = 0; i < result.rows.length; i++) {
-//       const row = result.rows[i];
-//       const checkInTime = row.check_in_time;
-//       const checkOutTime = row.check_out_time;
-//       NAME=row.name;
-//       const reason = row.reason.trim().toLowerCase(); // Trim and lower case the reason
-
-//       if (checkInTime && checkOutTime) {
-//         const checkInDateTime = new Date(`1970-01-01T${checkInTime}`);
-//         const checkOutDateTime = new Date(`1970-01-01T${checkOutTime}`);
-//         const diffMs = checkOutDateTime - checkInDateTime;
-//         const diffMinutes = diffMs / 1000 / 60;
-
-//         totalMinutes += diffMinutes;
-//       }
-
-//       if (checkOutTime && i < result.rows.length - 1) {
-//         const nextRow = result.rows[i + 1];
-//         const nextCheckInTime = nextRow.check_in_time;
-//         if (nextCheckInTime) {
-//           const checkOutDateTime = new Date(`1970-01-01T${checkOutTime}`);
-//           const nextCheckInDateTime = new Date(`1970-01-01T${nextCheckInTime}`);
-//           const diffMs = nextCheckInDateTime - checkOutDateTime;
-//           const diffMinutes = diffMs / 1000 / 60;
-
-//           console.log(`Reason: "${reason}", Diff Minutes: ${diffMinutes}`);
-
-//           if (reason === 'break') {
-//             breakMinutes += diffMinutes;
-//             console.log("BREAK")
-//           } else if (reason.includes('office')) { // Check if reason contains "office"
-//             officeWorkMinutes += diffMinutes;
-//             console.log("OFFICE WORK")
-//           } else {
-//             otherMinutes += diffMinutes;
-//             console.log("OTHER")
-//           }
-//         }
-//       }
-//     }
-
-//     const totalHours = Math.floor(totalMinutes / 60);
-//     const totalMinutesRemainder = totalMinutes % 60;
-
-//     const breakHours = Math.floor(breakMinutes / 60);
-//     const breakMinutesRemainder = breakMinutes % 60;
-
-//     const officeWorkHours = Math.floor(officeWorkMinutes / 60);
-//     const officeWorkMinutesRemainder = officeWorkMinutes % 60;
-
-//     const otherHours = Math.floor(otherMinutes / 60);
-//     const otherMinutesRemainder = otherMinutes % 60;
-   
-
-//     res.render('daily_working_hours_report', { 
-//       NAME, 
-//       date, 
-//       totalHours, 
-//       totalMinutesRemainder, 
-//       breakHours, 
-//       breakMinutesRemainder, 
-//       officeWorkHours, 
-//       officeWorkMinutesRemainder, 
-//       otherHours, 
-//       otherMinutesRemainder 
-//     });
-//   } catch (error) {
-//     console.error("Error executing query:", error);
-//     res.status(500).send("Internal Server Error");
-//   }
-// });
 app.post('/daily_working_hours', async (req, res) => {
   const { name, date } = req.body;
   const user = req.session.user;
@@ -985,8 +661,6 @@ app.post('/daily_working_hours', async (req, res) => {
   }
 });
 
-
-
 app.get("/check_in", async (req, res) => {
   user = req.session.user;
   console.log(user)
@@ -1043,52 +717,6 @@ app.get("/check_out", async (req, res) => {
     res.redirect("/");
   }
 });
-
-// app.post('/check_out', async (req, res) => {
-//   const user = req.session.user;
-//   const loginId = user.id;
-//   const { name, time, date, reason } = req.body;
-//   console.log("Time:", time);
-//   console.log("Reason:", reason);
-  
-//   try {
-   
-//     const [hours, minutes, seconds] = time.split(':');
-//     const formattedTime = `${hours}:${minutes}:${seconds}`;
-//     console.log("Formatted Time:", formattedTime);
-//     const selectQuery = `
-//       SELECT id FROM record
-//       WHERE name = $1 AND date = $2 AND id = $3 AND check_out_time IS NULL
-//       LIMIT 1;
-//     `;
-//     const selectValues = [name, date, loginId];
-    
-//     const selectResult = await pool.query(selectQuery, selectValues);
-
-//     if (selectResult.rows.length === 0) {
-//       res.status(400).send("Check-in record not found or already checked out.");
-//       return;
-//     }
-
-//     // Update query to set the check-out time and reason
-//     const updateQuery = `
-//       UPDATE record
-//       SET check_out_time = $1, reason = $2
-//       WHERE id = $3 AND date = $4 AND check_out_time IS NULL;
-//     `;
-//     console.log("Change Time:",formattedTime)
-//     const updateValues = [time, reason, loginId, date];
-
-//     await pool.query(updateQuery, updateValues);
-
-//     res.render("dashboard");
-//   } catch (error) {
-//     console.error("Error executing check-out query:", error);
-//     res.status(500).send("Internal Server Error");
-//   }
-// });
-
-////Add Employee
 
 app.post('/check_out', async (req, res) => {
   const user = req.session.user;
